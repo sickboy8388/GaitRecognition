6980490aa0cf95e3ddf180536873f76b40d98bdd
mattia pirro <mattiapirro@matt.local>
2017-03-15 16:36:47 +0100

    second test

--- GaitRecognition/ViewController.swift
+++ GaitRecognition/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+        print("hey")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
